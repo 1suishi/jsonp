873f138c63243dfe06ac10ec4aca89a3bd9d64bb
benniuo <1962323664@qq.com>
2015-12-17 18:02:39 +0800

    add Access-Control-Allow-Origin

--- appB.js
+++ appB.js
@@ -73,11 +73,9 @@ http.createServer(function(req,res){
                     +'</html>';
         res.write(html);
         res.end();
-
-        Access-Control-Allow-Origin: *
     }else if(urlPath === '/followers.json'){
         res.writeHead(200,{'Content-Type':'application/json;charset=utf-8'});
-        res.writeHead({'Access-Control-Allow-Origin':*});
+        res.writeHead({'Access-Control-Allow-Origin'*});
 
         var followers = {
                                 "users" : [
